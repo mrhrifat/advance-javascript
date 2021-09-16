ad0a24cf9d3a9041aefa3398f589c22b6f157e18
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-09-16 20:38:56 +0600

    Updating codes

--- ES6/src/scripts/Iterator.js
+++ ES6/src/scripts/Iterator.js
@@ -1,6 +1,42 @@
 // 151 - Iterator
+
+/*
+
+01) Iterator is an object
+02) Iterator can be done if the object is iterable
+03) Array, String is iterable
+04) Object is not iterable
+05) Iterator hold data is closure
+06) Iterator can start and stop anywhere when Iterator is running
+07) Every has two method 
+    01) Next
+    02) Done
+
+*/
+
 let arr = [1, 2, 3, 4, 5]
 
 for (let x of arr) {
     console.log(x)
 }
+
+// Create Iterator
+let createIterator = collection => {
+    let i = 0
+    return {
+        next() {
+            return {
+                done: i >= collection.length,
+                value: collection[i++]
+            }
+        }
+    }
+}
+let r1 = createIterator(arr)
+console.log(r1.next())
+console.log(r1.next())
+console.log(r1.next())
+console.log(r1.next())
+console.log(r1.next())
+console.log(r1.next())
+console.log(r1.next())
